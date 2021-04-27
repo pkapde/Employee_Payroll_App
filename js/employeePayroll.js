c64dbb7eff004b4e9b4383342cc3b3fdab5cd8ed
prashant <pkapde308@gmail.com>
2021-04-27 23:12:07 +0530

    Added:code to create employee payroll object at local storage

--- js/employeePayroll.js
+++ js/employeePayroll.js
@@ -29,10 +29,23 @@ const salary = document.querySelector("#salary");
 const save = () => {
   try {
       let employeePayrollData = createEmployeePayroll();
+      createAndUpdateStorage(employeePayrollData);
   } catch (e) {
       return;
   }
 }
+
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+  if(employeePayrollList != undefined){
+      employeePayrollList.push(employeePayrollData);
+  } else{
+      employeePayrollList = [employeePayrollData]
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem("EmployeePayrollList", JSON.stringyfy(employeePayrollList))
+}
+
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
   try {
